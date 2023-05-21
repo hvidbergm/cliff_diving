3b0b76f1332fe2c6e2c3e676c79494e2ddc2c114
Mads <hvidbergm@outlook.com>
2023-05-21 19:24:49 +0200

    Added spots to the map

--- utils/get_data.py
+++ utils/get_data.py
@@ -1,13 +1,14 @@
 import pandas as pd
 from dotenv import load_dotenv
 import os
+import json
 
 def get_markers():
     load_dotenv()
     key = "SHEET_ID"
     url = f'https://docs.google.com/spreadsheets/d/{os.getenv(key)}/export?format=csv'
-    column_names = ["timestamp", "lng", "lat", "title"]
-    return pd.read_csv(url, names=column_names, header=0).to_dict('records')
+    column_names = ["timestamp", "longitude", "latitude", "title", "description", "altitude", "directions", "img", "safety"]
+    return pd.read_csv(url, names=column_names, header=0).to_json(orient="records")
 
 def get_total_contributions():
-    return len(get_markers())
+    return len(json.loads(get_markers()))
